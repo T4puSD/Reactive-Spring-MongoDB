c38e5a6559fa23ff203dd8a2774cc71bcaf47b5b
tapu <tapu@rokomari.com>
2020-11-16 13:12:26 +0600

    update logic changed

--- src/main/java/com/tapusd/reactivemongodbexample/controller/ReactiveController.java
+++ src/main/java/com/tapusd/reactivemongodbexample/controller/ReactiveController.java
@@ -10,7 +10,6 @@ import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
-import reactor.util.function.Tuple2;
 
 import java.time.Duration;
 import java.time.LocalDateTime;
@@ -29,37 +28,31 @@ public class ReactiveController {
     }
 
     @GetMapping("/all")
-    public Flux<Employee> getAll(){
+    public Flux<Employee> getAll() {
         return employeeRepository.findAll();
     }
 
     @GetMapping("/{id}")
-    public Mono<Employee> getOne(@PathVariable String id){
+    public Mono<Employee> getOne(@PathVariable String id) {
         return employeeRepository.findById(id);
     }
 
     @PutMapping("/{id}")
-    public Mono<Employee> updateEmployee(@PathVariable String id, @RequestBody Employee employee){
-//        return employeeRepository.save(employee);
-        return employeeRepository.findById(id).flatMap(employee1 -> {
-            if(employee1.getId().equals(id)){
-                employee1.setName(employee.getName());
-                employee1.setSalary(employee.getSalary());
-                LOGGER.info("saving old emplohyee with updated value");
-                return employeeRepository.save(employee1);
-            } else {
-                LOGGER.info("new employee is getting saved");
-                employee.setId(id);
-                return employeeRepository.save(employee);
-            }
-        });
+    public Mono<Employee> updateEmployee(@PathVariable String id, @RequestBody Employee employee) {
+        employee.setId(id);
+        return employeeRepository.findById(id).flatMap(emp -> {
+            emp.setName(employee.getName());
+            emp.setSalary(employee.getSalary());
+            LOGGER.info("Saving old employee with updated value");
+            return employeeRepository.save(emp);
+        }).or(employeeRepository.save(employee));
     }
 
     @GetMapping(value = "/{id}/events", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
-    public Flux<EmployeDTO> getOneEvent(@PathVariable String id){
+    public Flux<EmployeDTO> getOneEvent(@PathVariable String id) {
         return employeeRepository.findById(id)
                 .flatMapMany(
-                        employee -> Flux.fromStream(Stream.generate(()-> new EmployeDTO(employee.getId(),employee.getName(),employee.getSalary(), LocalDateTime.now())))
+                        employee -> Flux.fromStream(Stream.generate(() -> new EmployeDTO(employee.getId(), employee.getName(), employee.getSalary(), LocalDateTime.now())))
                 ).delayElements(Duration.ofSeconds(5));
     }
 }
